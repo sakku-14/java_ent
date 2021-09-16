8bbcea181ec4abb57152d0821130d0812ca60f33
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-16 12:24:53 +0900

    [Improve] some

--- 2/work/Cast.java
+++ 2/work/Cast.java
@@ -1,8 +1,8 @@
 public class Cast {
     public static void main(String[] args) {
         int a = 10;
-        //double d = a;
-        double d = 3.15;
-        a = d;//コンパイル時にエラー
+        double d = a;
+        //double d = 3.15;
+        //a = d;//コンパイル時にエラー
     }
 }
