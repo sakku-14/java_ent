86190ded41365da7095274093f9b332942b38163
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-16 12:37:33 +0900

    [Improve] integer.parseint

--- 2/work/Function.java
+++ 2/work/Function.java
@@ -3,5 +3,11 @@ public class Function {
         int comp = Math.max(100, 50);
 
         System.out.println(comp);
+
+        int age;
+        String sAge = "29";
+
+        age = Integer.parseInt(sAge);
+        System.out.println("来年は" + (age + 1) + "歳です。");
     }
 }
