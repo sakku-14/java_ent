61ab438a271ffa37972189af309d40ad0e638148
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-17 08:20:24 +0900

    [Improve] ex2 in 5

--- 5/ex/Ex2.java
+++ 5/ex/Ex2.java
@@ -4,12 +4,19 @@ public class Ex2 {
         String address = "kumasan.04.24.14@gmail.com";
         String text = "We will see you at 10am in Shibuya. Please come to Hachiko. See you tomorrow.";
 
-        email(title, address, text);;
+        email(title, address, text);
+        email(address, text);
     }
 
     public static void email(String title, String address, String text) {
         System.out.println(address + "に、以下のメールを送信しました");
         System.out.println("件名:" + title);
+        System.out.println("本文:" + text);
+    }
+
+    public static void email(String address, String text) {
+        System.out.println(address + "に、以下のメールを送信しました");
+        System.out.println("件名:無題");
         System.out.println("本文:" + text);
     }
 }
