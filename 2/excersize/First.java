377760a8d03662003f966fde644b24467d60029d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-16 13:14:24 +0900

    [Improve] 2_ex

---
+++ 2/excersize/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        String str = "x + y = " + (x + y);
+
+        System.out.println(str);
+    }
+}
