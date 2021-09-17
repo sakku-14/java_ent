99b7c2906da672e0cf9714bab01d94ecad6372fe
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-17 09:27:33 +0900

    [Improve] add jar file

---
+++ 6/work/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+
+        System.out.println(CalcLogic.tasu(5, 9));
+        System.out.println(CalcLogic.kakeru(5, 9));
+    }
+}
---
+++ 6/work/CalcLogic.java
@@ -0,0 +1,8 @@
+public class CalcLogic {
+    public static int tasu(int a, int b) {
+        return (a + b);
+    }
+    public static int kakeru(int a, int b) {
+        return (a * b);
+    }
+}
