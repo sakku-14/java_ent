8b475ad76590c9d61d4a224009d0c53d084214fe
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-19 10:41:20 +0900

    [Improve] add wand in 13/ex

---
+++ 13/ex/Wand.java
@@ -0,0 +1,17 @@
+public class Wand {
+    private String name;
+    private double power;
+
+    public void setName(String name) {
+        this.name = n;
+    }
+    public void setPower(double power) {
+        this.power = p;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public double getPower() {
+        return this.power;
+    }
+}
