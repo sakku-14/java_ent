1be6c55fca3a25524a57224b42827ec1a960eca2
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-21 16:34:35 +0900

    [Improve] add better way in 15

--- 15/ex/ex4/Main.java
+++ 15/ex/ex4/Main.java
@@ -1,6 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
@@ -12,13 +13,17 @@ public class Main {
         c.add(Calendar.DAY_OF_MONTH, 100);
         d = c.getTime();
         System.out.println(d);
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.applyPattern("西暦yyyy年MM月dd日");
+        // SimpleDateFormat sdf = new SimpleDateFormat();
+        // sdf.applyPattern("西暦yyyy年MM月dd日");
+        //better way
+        SimpleDateFormat sdf = new SimpleDateFormat("西暦yyyy年MM月dd日");
         System.out.println(sdf.format(d));
 
         //Time API
         LocalDate ld = LocalDate.now();
         ld = ld.plusDays(100);
+        //another way
+        // ld = ld.plus(Period.ofDays(100));
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
         System.out.println(ld.format(dtf));
     }
