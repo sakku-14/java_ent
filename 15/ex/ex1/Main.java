4e3042de75ae1f35b4bbe19bda1eae22fbcbc89c
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-21 11:59:23 +0900

    [Imrpove] some in 15/work

---
+++ 15/ex/ex1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder s;
+
+        for (int i = 0; i < 100; i++) {
+            s.append(i + 1);
+            s.append(',')
+        }
+        System.out.println(s);
+    }
+}
--- 15/work/Main.java
+++ 15/work/Main.java
@@ -1,6 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -15,11 +16,31 @@ public class Main {
         // System.out.println(d);
 
         //SimpleDateFormat <-> Date
-        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd(E) HH:mm:ss");
-        Date now = new Date();
-        String s = f.format(now);
-        System.out.println(s);
-        Date d = f.parse(s);
-        System.out.println(d);
+        // SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd(E) HH:mm:ss");
+        // Date now = new Date();
+        // String s = f.format(now);
+        // System.out.println(s);
+        // Date d = f.parse(s);
+        // System.out.println(d);
+
+        //java.instant.*
+        // Instant i1 = Instant.now();
+        // System.out.println(i1);
+        // ZonedDateTime z1 = ZonedDateTime.now();
+        // System.out.println(z1);
+        // Instant i2 = z1.toInstant();
+        // System.out.println(i2);
+        // ZonedDateTime z3 = i2.atZone(ZoneId.of("Europe/London"));
+        // System.out.println(z3);
+
+        //LocalDateTime
+        LocalDateTime l1 = LocalDateTime.now();
+        LocalDateTime l2 = LocalDateTime.of(2020, 1, 1, 9, 5, 0, 0);
+        System.out.println(l1);
+        System.out.println(l2);
+        ZonedDateTime z4 = l2.atZone(ZoneId.of("Europe/London"));
+        System.out.println(z4);
+        LocalDateTime l3 = z4.toLocalDateTime();
+        System.out.println(l3);
     }
 }
