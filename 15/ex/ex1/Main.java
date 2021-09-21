f8ebbad57113b882ec5f1276951cf19af188ef91
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-21 12:20:48 +0900

    [Improve] add 15/ex/ex1

--- 15/ex/ex1/Main.java
+++ 15/ex/ex1/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder s;
+        StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < 100; i++) {
             s.append(i + 1);
-            s.append(',')
+            s.append(',');
         }
         System.out.println(s);
+        String str = new String(s);
+        String[] ans = new String[100];
+        ans = str.split(",");
+        for (String string : ans) {
+            System.out.println(string);
+        }
     }
 }
