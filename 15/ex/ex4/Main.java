6f53b7a3f8480d62e6374aa88717716b3c4daefc
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-21 16:29:48 +0900

    [Improve] fin 15

---
+++ 15/ex/ex4/Main.java
@@ -0,0 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        //Date-Calendar-SimpleDateFormat
+        Date d = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+        c.add(Calendar.DAY_OF_MONTH, 100);
+        d = c.getTime();
+        System.out.println(d);
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        sdf.applyPattern("西暦yyyy年MM月dd日");
+        System.out.println(sdf.format(d));
+
+        //Time API
+        LocalDate ld = LocalDate.now();
+        ld = ld.plusDays(100);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+        System.out.println(ld.format(dtf));
+    }
+}
