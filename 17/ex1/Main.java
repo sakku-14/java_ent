38e2198b5f09858fde79e9c84d0343afd8c33d94
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-22 09:29:48 +0900

    [Improve] fin 17

--- 17/ex1/Main.java
+++ 17/ex1/Main.java
@@ -1,23 +1,27 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 import jdk.jfr.StackTrace;
 
 public class Main {
-    public static void main(String[] args) {
-        String s = null;
-        try {
-            System.out.println(s.length());
-        } catch (NullPointerException e) {
-            System.err.println("NullPointerException例外をcatchしました");
-            System.err.println("--スタックとれーす--");
-            e.printStackTrace();
-            System.err.println("-----------------");
-        }
+    public static void main(String[] args) throws IOException {
+        throw new IOException("エラー投げるよー");
+        // String s = null;
+        // try {
+        //     System.out.println(s.length());
+        // } catch (NullPointerException e) {
+        //     System.err.println("NullPointerException例外をcatchしました");
+        //     System.err.println("--スタックとれーす--");
+        //     e.printStackTrace();
+        //     System.err.println("-----------------");
+        // }
 
-        try {
-            int x = Integer.parseInt("三");
-        } catch (NumberFormatException e) {
-            System.err.println("********Integer.parseInt*********");
-            e.printStackTrace();
-            System.err.println("*********************************");
-        }
+        // try {
+        //     int x = Integer.parseInt("三");
+        // } catch (NumberFormatException e) {
+        //     System.err.println("********Integer.parseInt*********");
+        //     e.printStackTrace();
+        //     System.err.println("*********************************");
+        // }
     }
 }
