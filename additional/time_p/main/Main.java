8738baf4f496dc84e184c8a1373a8157b1d885a0
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-23 21:28:41 +0900

    [Improve] add calendar

--- additional/time_p/main/Main.java
+++ additional/time_p/main/Main.java
@@ -1,9 +1,14 @@
 import java.util.Date;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("**************Date***************");
         Date d = new Date();
         System.out.println("Date型: [" + d + "], longで基準からのミリ秒: [" + d.getTime() + "]");
+
+        System.out.println("**************Calendar***************");
+        Calendar c = Calendar.getInstance();
+        System.out.println("Calendar型: [" + c + "]");
     }
 }
