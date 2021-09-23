113b165fd46ff846687ffc76d55b8c74f2251cdb
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-23 22:57:59 +0900

    [Improve] simpledateformat

--- additional/time_p/main/Main.java
+++ additional/time_p/main/Main.java
@@ -1,14 +1,23 @@
 import java.util.Date;
 import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
+        //Date
         System.out.println("**************Date***************");
         Date d = new Date();
         System.out.println("Date型: [" + d + "], longで基準からのミリ秒: [" + d.getTime() + "]");
 
+        //Calendar型は各値を単独で取ることができる。
         System.out.println("**************Calendar***************");
         Calendar c = Calendar.getInstance();
         System.out.println("Calendar型: [" + c + "]");
+
+        //SimpleDateFormat
+        System.out.println("*************SimpleDateFormat****************");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        String ret = sdf.format(d);
+        System.out.println("SimpleDateFormat型: [" + sdf + "], method: [" + ret + "]");
     }
 }
