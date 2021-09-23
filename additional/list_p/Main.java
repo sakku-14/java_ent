69e49fbf4dbf4d66ca0b0ac324705fd1b1cc093a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-23 09:22:40 +0900

    [Improve] about list

--- additional/list_p/Main.java
+++ additional/list_p/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 class Main {
     public static void main(String[] args) {
@@ -15,5 +17,25 @@ class Main {
         al.remove(4);
 
         System.out.println("al.size: " + al.size());
+
+        al.add(2, "Keisuke");
+        al.add(3, "Seiko");
+
+        System.out.println("al.size: " + al.size());
+
+        for (String name : al) {
+            System.out.println("Name1: " + name);
+        }
+
+        Iterator<String> i = al.iterator();
+        while (i.hasNext())
+        {
+            String name = i.next();
+            System.out.println("Name2: " + name);
+        }
+        Iterator<String> i2 = al.iterator();
+        Consumer<String> act = (name) -> { System.out.println("Name3: " + name); };
+        // al.forEachRemaining(act);
+        i2.forEachRemaining(act);
     }
 }
