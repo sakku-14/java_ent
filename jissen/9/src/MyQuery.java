b32ecf2dec60fe109373d6a00e8d28b0b573f640
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-25 22:49:19 +0900

    [Imrove] add some and add comment

--- jissen/9/src/MyQuery.java
+++ jissen/9/src/MyQuery.java
@@ -1,14 +1,13 @@
 import java.sql.*;
 
 public class MyQuery {
-
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("ドライバのロードに失敗しました。");
 		}
-		
+		//トランザクション処理をやる！
 		Connection con = null;
 		try {
 			//検索
