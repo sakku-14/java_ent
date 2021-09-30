d704e04efcd13314057e4a3314f4450022f5a171
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-30 21:37:31 +0900

    [Improve] some

--- jissen/9/src/MyQuery.java
+++ jissen/9/src/MyQuery.java
@@ -12,6 +12,7 @@ public class MyQuery {
 		try {
 			//検索
 			con = DriverManager.getConnection("jdbc:mysql://localhost/sukkiri", "root", null);
+			con.setAutoCommit(false);
 			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM ITEMS");
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
@@ -19,13 +20,12 @@ public class MyQuery {
 			}
 			rs.close();
 			pstmt.close();
-			
 			//追加・更新・削除
 			//**********追加**************
 //			PreparedStatement pstmt2 = con.prepareStatement("INSERT INTO ITEMS VALUES (?, ?, ?);");
-//			pstmt2.setString(1, "銅のつるぎ");
-//			pstmt2.setInt(2, 10);
-//			pstmt2.setInt(3, 15);
+//			pstmt2.setString(1, "ポーション");
+//			pstmt2.setInt(2, 8);
+//			pstmt2.setInt(3, 4);
 //			int r = pstmt2.executeUpdate();
 //			if (r != 0) {
 //				System.out.println(r + "件の道具を追加しました");
@@ -54,8 +54,13 @@ public class MyQuery {
 //				System.out.println("道具の削除に失敗しました");
 //			}
 //			pstmt4.close();
+			con.commit();
 		} catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
 		} finally {
 			if (con != null) {
 				try {
