6a932b3b21b54a4434e4846f476e2654203f1fe0
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-10-08 20:11:21 +0900

    [Improve] add test dir for check act

---
+++ test/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+import main.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Test t = new Test(3);
+        System.out.println(t.getNum());
+    }
+}
---
+++ test/main/Test.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Test {
+    private int num;
+
+    public Test(int n) {
+        this.num = n;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
